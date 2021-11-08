d6ca1faf6a5db027d0b1bdff31a14a34d7b1f065
anshita066 <anshita_066@yahoo.in>
2021-11-08 16:14:55 +0530

    uploading project

--- app/src/main/java/com/example/androidbootcamp/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/androidbootcamp/RecyclerViewAdapter.kt
@@ -1,39 +1,54 @@
 package com.example.androidbootcamp
 
-
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.androidbootcamp.databinding.ItemViewBinding
+import com.example.androidbootcamp.roomdb.UserEntity
+import kotlinx.android.synthetic.main.row_item.view.*
 
-class RecyclerViewAdapter(private val data:List<DataModel>): RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>(){
+class RecyclerViewAdapter (val listener:RowClickListener):RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>(){
 
+    var items = ArrayList<UserEntity>()
 
-    inner class MyViewHolder(val binding: ItemViewBinding):RecyclerView.ViewHolder(binding.root){
-        fun bind(item:DataModel){
-            with(binding){
-                textView.text=item.name
-                textView1.text=item.email
-                textView2.text=item.phoneNumber.toString()
-                textView3.text=item.address
-            }
-
-
-
-        }
+    fun setListData(data:ArrayList<UserEntity>){
+        this.items = data
     }
-
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val listItemBinding = ItemViewBinding.inflate(inflater,parent,false)
-        return MyViewHolder(listItemBinding)
+        val inflater = LayoutInflater.from(parent.context).inflate(R.layout.row_item, parent, false)
+        return MyViewHolder(inflater , listener)
     }
 
-    override fun onBindViewHolder(holder:MyViewHolder, position: Int) {
-        holder.bind(data[position])
+    override fun onBindViewHolder(holder: RecyclerViewAdapter.MyViewHolder, position: Int) {
+        holder.itemView.setOnClickListener{
+            listener.onItemClickListener(items[position])
+        }
+        holder.bind(items[position])
+
     }
 
     override fun getItemCount(): Int {
-        return data.size
+
+        return items.size   }
+
+    class MyViewHolder(view: View , val listener: RowClickListener) :RecyclerView.ViewHolder(view){
+
+        val tvName = view.tvName
+        val tvEmail = view.tvEmail
+        val tvphone = view.tvPhone
+
+        val deleteUserId = view.deleteUserID
+        fun bind(data:UserEntity){
+            tvName.text = data.name
+            tvEmail.text = data.email
+            tvphone.text = data.phone
+            deleteUserId.setOnClickListener{
+                listener.onDeleteUserClick(data)
+            }
+        }
+    }
+    interface RowClickListener{
+        fun onDeleteUserClick(user:UserEntity)
+        fun onItemClickListener(user: UserEntity)
     }
 }
